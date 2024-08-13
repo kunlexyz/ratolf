0bcfcd2ec1d718d4da13a4cc70d77057c3878748
kunlexyz <bobbykay007@gmail.com>
2024-08-13 13:23:49 +0100

    Updated

--- js/reverse_2.js
+++ js/reverse_2.js
@@ -4,14 +4,18 @@
 window.onload = function(){
 
     /*codes for mini Sliders*/
-    var pic2=['save_africa2.jpg','save_africa.jpg'];
-    p2Wheel=0;
+    var pic2=['save_africa2.jpg','save_africa.jpg'],
+    p2Wheel=0,
+    slide_height=500;
     function slideMini(){
-        var holda1='<div class="boxAdsC"><img src="image/'+pic2[p2Wheel]+'" id="adx1" class="boxAdsImage" style="margin:0px" />';
+        var holda1='<div class="boxAdsC" style="height:'+slide_height+'px;"><img src="image/'+pic2[p2Wheel]+'" id="adx1" class="boxAdsImage" style="margin:0px;height:'+slide_height+'px;" />';
         p2Wheel++;
+        console.log(window.innerWidth);
+        if(window.innerWidth > 1360){slide_height=700}
         if(p2Wheel==pic2.length){p2Wheel=0;}
-        var holda2='<img src="image/'+pic2[p2Wheel]+'" class="boxAdsImage" /></div>';
+        var holda2='<img src="image/'+pic2[p2Wheel]+'" class="boxAdsImage" style="height:'+slide_height+'px;" /></div>';
         document.getElementById('boxAds1').innerHTML=holda1+holda2;
+        document.getElementById('boxAds1').style.height=slide_height+'px';
         $('#adx1').animate({'margin-left': '-25%'}, 3000);
         setTimeout(slideMini,6000);
     }slideMini();
