71db1df11850a1c09a1d86ceed0a31e5c5f14e3d
kunlexyz <bobbykay007@gmail.com>
2024-09-05 22:21:41 +0100

    lots of changes Sept 05

--- js/reverse_2.js
+++ js/reverse_2.js
@@ -11,7 +11,7 @@ window.onload = function(){
         var holda1='<div class="boxAdsC" style="height:'+slide_height+'px;"><img src="image/'+pic2[p2Wheel]+'" id="adx1" class="boxAdsImage" style="margin:0px;height:'+slide_height+'px;" />';
         p2Wheel++;
         console.log(window.innerWidth);
-        if(window.innerWidth > 1360){slide_height=700}else{slide_height=500}
+        if(window.innerWidth > 1550){slide_height=700}else{slide_height=500}
         if(p2Wheel==pic2.length){p2Wheel=0;}
         var holda2='<img src="image/'+pic2[p2Wheel]+'" class="boxAdsImage" style="height:'+slide_height+'px;" /></div>';
         document.getElementById('boxAds1').innerHTML=holda1+holda2;
